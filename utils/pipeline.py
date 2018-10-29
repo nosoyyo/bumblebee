507d43cb9da667ceb6be8c3b89a24365b2445973
nosoyyo <oyyoson@gmail.com>
2018-10-30 00:14:20 +0800

    added pipeline & base model from rabona

---
+++ utils/pipeline.py
@@ -0,0 +1,167 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+# @absurdity @real @mini
+__author__ = 'nosoyyo'
+__version__ = {'0.1': '2018.01',
+               '0.2': '2018.02',
+               '0.3': '2018.05.23',
+               '0.4': '2018.05.31',
+               '0.41': '2018.10.29'
+               }
+
+# 0.1 basically setup
+# 0.2 base class changed into singleton mode; added user/pwd auth;
+# 0.3 removed singleton mode, added basic CRUD method wrappers
+# 0.4 add `ls` behaviour customization
+# 0.5 implemented on BumbleBee; exchanged lots of f-strings;
+
+import os
+import logging
+import pymongo
+from bson.objectid import ObjectId
+from pymongo.collection import Collection
+
+from .exception import InvalidCollectionError
+
+
+settings = {}
+settings['MONGODB_SERVER'] = os.environ.get('LOCAL_MONGODB_SERVER')
+settings['MONGODB_PORT'] = int(os.environ.get('LOCAL_MONGODB_PORT'))
+settings['MONGODB_USERNAME'] = os.environ.get('LOCAL_MONGODB_USERNAME')
+settings['MONGODB_PASSWORD'] = os.environ.get('LOCAL_MONGODB_PASSWORD')
+settings['MONGODB_BUMBLEBEE_DB'] = 'bumblebee'
+settings['MONGODB_INIT_COL'] = 'testcol'
+
+# init
+if 'log' not in os.listdir():
+    os.mkdir('log')
+
+logging.basicConfig(
+    filename='var/log/mongodb.log',
+    level=logging.INFO,
+    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s')
+
+
+# ==================
+# MongoDB quickstart
+# ==================
+class MongoDBPipeline():
+    client = pymongo.MongoClient(
+        settings['MONGODB_SERVER'],
+        settings['MONGODB_PORT'],
+    )
+    db = client.get_database(settings['MONGODB_RABONA_DB'])
+    auth = db.authenticate(
+        settings['MONGODB_USERNAME'],
+        settings['MONGODB_PASSWORD']
+    )
+    col = db.get_collection(settings['MONGODB_INIT_COL'])
+
+    def setDB(self, dbname):
+        self.db = self.client.get_database(dbname)
+        return self
+
+    def setCol(self, colname, dbname=None):
+        if dbname:
+            self.db = self.client.get_database(dbname)
+        return self.db.get_collection(colname)
+
+    def dealWithCol(self, _col=None) -> Collection:
+        try:
+            if not _col:
+                col = self.col
+            elif isinstance(_col, Collection):
+                col = _col
+            elif isinstance(_col, str):
+                col = self.setCol(_col)
+            else:
+                raise InvalidCollectionError(_col)
+            return col
+        except Exception as e:
+            logging.error(f'dealWithCol raises {e}')
+
+    def insert(self, doc: dict, col: str=None) -> ObjectId:
+        col = self.dealWithCol(col)
+        try:
+            oid = col.insert_one(doc).inserted_id
+            if isinstance(oid, ObjectId):
+                logging.debug(f'doc {doc} inserted into {col.name}')
+                return oid
+        except Exception as e:
+            logging.error(f'pipeline.insert() raises {e}')
+
+    def ls(self, arg=None, col=None, **kwargs):
+        col = self.dealWithCol(col)
+
+        if arg is None:
+            return self.db.collection_names()
+        elif isinstance(arg, str):
+            col = self.dealWithCol(arg)
+            return [item for item in col.find()]
+        elif isinstance(arg, int) and col is None:
+            col = self.ls()[arg]
+            return self.ls(col)
+        elif isinstance(arg, dict):
+            logging.debug(f'looking for {arg} in "{col.name}"')
+            return [item for item in col.find(arg)]
+        elif isinstance(arg, ObjectId):
+            return col.find_one({'_id': arg})
+        elif col.name in self.custom_ls_behaviour_col:
+            return self.custom_ls(arg=arg, col=col, **kwargs)
+        elif kwargs:
+            # TODO
+            return col.find_one(kwargs)
+
+    def update(self, oid: ObjectId, doc: dict, col: str=None) -> bool:
+        col = self.dealWithCol(col)
+
+        try:
+            result = col.update_one({'_id': oid}, {"$set": doc},)
+            if result.modified_count:
+                logging.debug(f'updated {doc} in {col.name}, result[{result}]')
+                    doc, col.name, result))
+                flag=True
+            else:
+                if result.matched_count:
+                    logging.debug(
+                        f'{doc} not updated in {col.name}, input same to output.')
+                    flag=False
+                else:
+                    logging.debug(
+                        f'{doc} not updated in {col.name}, nothing matches input.')
+                    flag=False
+
+            return flag
+        except Exception as e:
+            logging.error(f'm.update raises {e}')
+            return False
+
+    def rm(self, arg, col=None):
+        '''
+        Only supports deleting by oid for now.
+
+        :param oid: `bson.objectid.ObjectId`
+        '''
+        col=self.dealWithCol(col)
+
+        if isinstance(arg, ObjectId):
+            try:
+                result=bool(col.delete_one(
+                    {'_id': arg}).raw_result['n']) or False
+                logging.info(f'deleting {arg} in {col.name}, result[{result}]')
+                return result
+            except Exception as e:
+                logging.error(
+                    f'm.rm raises {e} during deleting by ObjectId'
+                return False
+        elif isinstance(arg, dict):
+            try:
+                if '_id' in arg.keys() and isinstance(arg['_id'], ObjectId):
+                    return self.rm(arg['_id'], col)
+                else:
+                    return False
+            except Exception as e:
+                logging.error(f'm.rm raises {e} during deleting doc')
+                return False
+        else:
+            return False
