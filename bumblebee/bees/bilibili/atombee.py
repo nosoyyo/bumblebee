bc09570c1e8845b70de44714e548533127c6852c
Paul Carino <paulcarinocrafts@gmail.com>
2018-11-10 20:35:03 +0800

    split bilibee into 3 parts

--- bumblebee/bees/bilibili/atombee.py
+++ bumblebee/bees/bilibili/atombee.py
@@ -1,35 +1,12 @@
-__doc__ = 'baidubce 2to3 by nosoyyo'
-__version__ = 'baidubce for py3 ver 2c374'
-
 import os
 import json
 
-from bees import AbstractBee
 from config import Bilibili
+from bees import AbstractBee
+from utils import SelfAssemlingClass
 from baidubce.services.bos.bos_client import BosClient
 from baidubce.auth.bce_credentials import BceCredentials
 from baidubce.bce_client_configuration import BceClientConfiguration
-
-'''
-DEBUG STUFF
-file = '/Users/nosoyyo/downloads/test.mp4'
-
-AccessKeyId = "0e84cc78e32111e89487cd9f7fafef9f"
-Expiration = "2018-11-08T18:39:35Z"
-SecretAccessKey = "f8505e7ada80435daee2d5cfb7e0b5fc"
-SessionToken = "MjUzZjQzNTY4OTE0NDRkNjg3N2E4YzJhZTc4YmU5ZDh8AAAAAFICAADsk6iYCf1SJeNajwUkr57prifDYpaiXmSacR9E3HAEtQ05Xe6qn8MD3quYBprPAstiIqA+noOnWIk8UzoueAdTLWXEOhO4SJCrHuXuL/nR46zk4bgx7dakVETkfAYwa+0MRoPnCeRbvDgjDxh+e/okFIJ5m/BX/0VbI5pKFDaznU4mP2ZR3evxZ2kuxv7NIW40w1FUwehBNlZ1lMBmZRVkbbEL8pvRtieczhtVlMkkEytKbFs0OiuDBQrCpTdeJGZ5rWA61JPvBpcY8p2W5Sh/jegr6nGIKzdz826YCDOxtJ6pXBrXmrKaHW3rppgcnw3fp1ydS+uBQfQ7EYtl77MkuC9LQxfp72l/44ejXvHLZAblpjwHByCzzEZ5p1GcvFSiHSNcVfPGDdmdt0Gd/FfWGAmf7RM5aHPuFU8sQzX1vuEE27zv5Bp6TxJpYV0sPcn4zeE0jJDkvmMiJ2X5xlAz"
-bili_filename = "i181108bozdu9w84w4u2v34ug6zn9qtu"
-key = "i181108bozdu9w84w4u2v34ug6zn9qtu.mp4"
-biz_id = 62315505
-bucket = "bvcupcdnbosxg"
-endpoint = "hk-2.bcebos.com"
-fetch_url = "//upos-hz-upcdnws.acgvideo.com/ugc/i181108bozdu9w84w4u2v34ug6zn9qtu.mp4?name=clip.mp4&fetch=&output=json&profile=ugcupos%2Ffetch&biz_id=62315505"
-'''
-
-
-class SelfAssemlingClass():
-    def __init__(self, doc):
-        self.__dict__ = doc
 
 
 class PreuploadResp(SelfAssemlingClass):
@@ -40,38 +17,14 @@ class PutObjectResp(SelfAssemlingClass):
     pass
 
 
-class BiliBee():
-    '''
-    For upload & add single file upon bilibili.com
-    '''
+class BiliAtomBee():
 
     config = Bilibili()
-    cu = os.getlogin()
-    config.full_path = f'/Users/{cu}/{config.default_dir}'
     bee = AbstractBee(config)
     bee.headers = config.headers.copy()
-    preupload_params = config.preupload_params
 
-    def __init__(self, file_name):
-        '''
-        : param file_name: looks like 'a42b4aa9aa1a47ad8bb2c02a57b60482.mp4'
-        '''
-        self.URI = file_name.replace('.mp4', '')
-        self.full_name = f'{self.config.full_path}/{file_name}'
-
-    def uploadMain(self):
+    def __init__(self, file_obj):
         pass
-
-    def checkVideos(self, path: str = None) -> list:
-        '''
-        NOT FOR THIS CLASS
-        SHOULD BE MOVED TO BATCH CLASS
-        check the default path, collect videos for uploading.
-
-        : return: a `list` of video file path(s)
-        '''
-        _dir = path or self.config.full_path
-        return [f for f in os.listdir(_dir) if f.endswith('.mp4')]
 
     def upload(self):
 
@@ -81,6 +34,9 @@ class BiliBee():
         po_resp = self.putObject(middle)
         po_result = PutObjectResp(json.loads(po_resp.text))
         self.postPO(middle)
+
+    def uploadMain(self):
+        pass
 
     def preUpload(self, full_name) -> json:
         '''
--- bumblebee/utils/__init__.py
+++ bumblebee/utils/__init__.py
@@ -4,3 +4,8 @@ from .pipeline import MongoDBPipeline
 from .sigmaactions import sigmaActions
 from .pinsheaders import pins_headers
 from .sumchars import sumChars
+
+
+class SelfAssemlingClass():
+    def __init__(self, doc):
+        self.__dict__ = doc
